cbbfe5f23cebcca906698d984c5c1f4b32f7128d
Aradhana Singh <aradhana971127@gmail.com>
2021-10-08 13:49:53 +0530

    Updating this file

--- First.java
+++ First.java
@@ -1,5 +1,9 @@
 class First{  
     public static void main(String args[]){  
+        int x;
+          public static final int SOME_CONSTANT = 0;     // Compliant - constants are not checked
+
+    public String firstName; 
      System.out.println("Hello Java");  
      System.out.println("Creating PR1");
     }  
