825b892f2029388919b26a6fe2c6bf6338d980c7
Aradhana Singh <aradhana971127@gmail.com>
2022-02-28 01:37:08 +0530

    Update First.java

--- src/main/First.java
+++ src/main/First.java
@@ -1,7 +1,7 @@
 class First{  
     public static void main(String args[]){  
         int a;
-        a ++;
+        a++;
      System.out.println("Hello Java");  
      System.out.println("Creating PR1");
     }  
