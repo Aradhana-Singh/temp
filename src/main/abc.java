da1d30de1061c84496e324a361e014195264b4ea
Aradhana Singh <aradhana971127@gmail.com>
2022-02-28 01:38:13 +0530

    Update abc.java

--- src/main/abc.java
+++ src/main/abc.java
@@ -1,3 +1,11 @@
 public class abc {
+static void myMethod() {
+    System.out.println("I just got executed!");
+  }
 
+  public static void main(String[] args) {
+    myMethod();
+    myMethod();
+    myMethod();
+  }
 }
