7b29d4f47b2e53b66f40193df5527c34d089a79f
Aradhana Singh <aradhana971127@gmail.com>
2022-02-28 01:54:16 +0530

    Update abc.java

--- src/main/abc.java
+++ src/main/abc.java
@@ -3,8 +3,6 @@ static void myMethod() {
     System.out.println("I just got executed!");
   }
   public static void main(String[] args) {
-      int a = 0;
-      a = 1/a;
     myMethod();
     myMethod();
     myMethod();
