f036d8fb30f262bef6d8db2f23964c0cd8f666bc
Aradhana Singh <aradhana971127@gmail.com>
2022-08-09 15:41:57 +0530

    Create test57.java

---
+++ test57.java
@@ -0,0 +1,17 @@
+public class test57 {
+ 
+  public static void Method() {
+
+        final String string1="some_random_string";
+
+        final String string2="some_random_string";
+
+        if ( string1 != null ) {  
+          int a = 1;
+        }
+
+        if( string1!=string2) {
+           int b = 1;
+        }
+    }
+}
